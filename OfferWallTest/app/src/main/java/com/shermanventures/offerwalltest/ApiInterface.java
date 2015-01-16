b090dc9ebbcaccd3c8c1ac228d490cd5196e0df9
aricos <astriglis@gmail.com>
2015-01-16 15:50:40 +0200

    remove  the code

--- OfferWallTest/app/src/main/java/com/shermanventures/offerwalltest/ApiInterface.java
+++
@@ -1,7 +0,0 @@
-package com.shermanventures.offerwalltest;
-
-/**
- * Created by aris on 15/12/2014.
- */
-public class ApiInterface {
-}
--- OfferWallTest/app/src/main/java/com/shermanventures/offerwalltest/OnGcmRegisteredListener.java
+++
@@ -1,9 +0,0 @@
-package com.shermanventures.offerwalltest;
-
-/**
- * Created by aris on 15/01/2015.
- */
-public interface OnGcmRegisteredListener {
-    public void OnGcmRegistered(String registrationId);
-    public void OnGcmRegistrationError(Exception exception);
-}
